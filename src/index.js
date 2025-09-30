5d0432893696d64ba14e1fbb4a006d74333a890b
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-09-30 22:19:29 +0500

    JSON URL and STATIC AND COOKIES middle ware added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
 
 dotenv.config({
     path: "./.env"
 });
+const app = express();
 
 // Connecting to database
-connectDB();
+connectDB()
+.then(
+    () => app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+)
+.catch((error) => {
+    console.error("Error in connecting to database", error);
+    process.exit(1);
+})

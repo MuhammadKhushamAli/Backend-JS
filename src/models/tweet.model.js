0efae53e7f8e5d92a171b443c3c4dd6732c4fc02
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-10-16 22:59:40 +0500

    Tweet Model Added

---
+++ src/models/tweet.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const tweetSchema = mongoose.Schema(
+    {
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        constent: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);

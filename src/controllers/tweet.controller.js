dc05a1a36eb08d46ab807fb198d5a6e885bb9aa1
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-10-17 23:03:44 +0500

    Get User Added

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,47 +2,53 @@ import { promiseAsyncHandler } from '../utils/promiseAsyncHandler.js';
 import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { Tweet } from '../models/tweet.model.js';
+import { User } from '../models/user.model.js';
 
 export const createTweet = promiseAsyncHandler(async (req, res) => {
     const { content } = req?.body;
-    if ( !(content.trim()) ) throw new ApiError(400, "Tweet content is required");
+    if (!(content.trim())) throw new ApiError(400, "Tweet content is required");
 
     const user = req?.user;
-    if( !user ) throw new ApiError(500, "Unable to identify user");
+    if (!user) throw new ApiError(500, "Unable to identify user");
 
     const tweet = await Tweet.create({
         owner: user._id,
         content: content.trim()
     });
 
-    if( !tweet ) throw new ApiError(500, "Unable to create tweet");
+    if (!tweet) throw new ApiError(500, "Unable to create tweet");
 
     return res.status(200)
-    .json(
-        new ApiResponse(
-            200,
-            "Tweet created successfully",
-            tweet
-        )
-    );
+        .json(
+            new ApiResponse(
+                200,
+                "Tweet created successfully",
+                tweet
+            )
+        );
 });
 
 export const getUserTweets = promiseAsyncHandler(async (req, res) => {
-    const userName = req?.params;
+    const { userName } = req?.params;
+    if (!userName) throw new ApiError(400, "Username is required");
 
-    // To be implemented: Fetch tweets by username
-    if( !userName ) throw new ApiError(500, "Unable to identify user");
+    const user = await User.findOne(
+        {
+            userName: userName.trim()
+        }
+    );
+    if (!user) throw new ApiError(404, "User not found");
 
-    const tweets = await Tweet.find({owner: userName?._id}).sort({createdAt: -1}).lean();
+    const tweets = await Tweet.find({ owner: user?._id }).sort({ createdAt: -1 }).lean();
 
-    if ( !tweets ) throw new ApiError(500, "Unable to fetch tweets");
+    if (!tweets) throw new ApiError(500, "Unable to fetch tweets");
 
     return res.status(200)
-    .json(
-        new ApiResponse(
-            200,
-            "Tweets fetched successfully",
-            tweets
-        )
-    );
+        .json(
+            new ApiResponse(
+                200,
+                "Tweets fetched successfully",
+                tweets
+            )
+        );
 });
--- src/routes/tweet.router.js
+++ src/routes/tweet.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
     createTweet,
+    getUserTweets,
 } from "../controllers/tweet.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -8,6 +9,6 @@ const router = Router();
 
 // Secure Route
 router.route("/tweet").post(verifyJWT, createTweet);
-router.route("/getTweets").get()
+router.route("/get-tweets/:userName").get(verifyJWT, getUserTweets);
 
 export default router;

064a2de3d06cc5afd778c30c0d51a53c34d4b76c
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-10-21 19:06:13 +0500

    tweeter controller added subscription model corrected

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,45 +1,43 @@
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { promiseAsyncHandler } from "../utils/promiseAsyncHandler";
+import { Video } from "../models/video.models";
+import mongoose from "mongoose";
+import { User } from "../models/user.models";
 
 export const getAllVideos = promiseAsyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userName} = req?.query;
+    let { page = 1, limit = 10, query, sortBy, sortType, userName } = req?.query;
 
+    page = parseInt(page, 10);
+    limit = parseInt(limit, 10);
+    if (query?.length > 100) throw new ApiError(400, "Query is too long");
     if (!userName) throw new ApiError(400, "UserName Must be Required");
-    if(page < 0) throw new ApiError(400, "Page must be greater than or equal to 0");
-    if(limit < 1) throw new ApiError(400, "Limit must be greater than 0");
+    if (page < 0) throw new ApiError(400, "Page must be greater than or equal to 0");
+    if (limit < 1) throw new ApiError(400, "Limit must be greater than 0");
 
     const user = await User.findOne({
         userName: userName.trim()
     });
-    if(!user) throw new ApiError(404, "User not found");
+    if (!user) throw new ApiError(404, "User not found");
 
     const actorUser = req?.user;
-    if(!actorUser) throw new ApiError(401, "Unauthorized Access");
+    if (!actorUser) throw new ApiError(401, "Unauthorized Access");
 
-    const video = await Video.aggregate([
+    let isOnlyPublicVideos = true;
+    if (actorUser?._id.equals(user?._id)) {
+        isOnlyPublicVideos = false;
+    }
+
+    const videoAggregate = Video.aggregate([
         {
             $match: {
-                $and: [
-                    {
-                        owner: new mongoose.Types.ObjectId(user?._id)
-                    },
-                    {
-                        title: {
-                            $and: [
-                                {$regex: query?.trim() || ""},
-                                {$options: "i"}
-                            ]
-                        }
-                    },
-                    {
-                        $cond: {
-                            if: {$ne: user?._id.equals(actorUser?._id)},
-                            then: {isPublished: true}
-                        }
-                    }
-                ]
-            }
+                owner: new mongoose.Types.ObjectId(user?._id),
+                title: {
+                    $regex: query?.trim() || "",
+                    $options: "i"
+                },
+                ...(isOnlyPublicVideos && { isPublished: true })
+            },
         },
         {
             $lookup: {
@@ -67,26 +65,109 @@ export const getAllVideos = promiseAsyncHandler(async (req, res) => {
         },
         {
             $sort: {
-                sortBy: sortType === "desc" ? -1 : 1
+                [sortBy || "createdAt"]: sortType === "desc" ? -1 : 1
             }
         }
     ]);
-    if(!video) throw new ApiError(500, "Unable to fetch videos");
+    if (!videoAggregate) throw new ApiError(500, "Aggregation failed");
 
     const options = {
         page,
         limit
     };
 
-    const paginatedVideos = await Video.aggregatePaginate(video, options);
-    if(!paginatedVideos) throw new ApiError(500, "Unable to fetch videos");
+    const paginatedVideos = await Video.aggregatePaginate(videoAggregate, options);
+    if (!paginatedVideos) throw new ApiError(500, "Unable to fetch videos");
 
     return res.status(200)
-    .json(
-        new ApiResponse(
-            200,
-            "Videos fetched successfully",
-            paginatedVideos
+        .json(
+            new ApiResponse(
+                200,
+                "Videos fetched successfully",
+                paginatedVideos
+            )
         )
+});
+
+export const getVideoById = promiseAsyncHandler(async (req, res) => {
+    const { videoId } = req?.params;
+
+    if (!videoId) throw new ApiError(400, "Video ID is required");
+    if (!mongoose.isValidObjectId(videoId)) throw new ApiError(400, "Invalid Video ID");
+
+    const video = await Video.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribers",
+                        }
+                    },
+                    {
+                        $addFields: {
+                            isSubscribed: {
+                                $cond: {
+                                    if: { $in: [req?.user?._id, "$subscribers.subscriber"] },
+                                    then: true,
+                                    else: false
+                                }
+                            },
+                            subscribers: {
+                                $size: "$subscribers"
+                            },
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner"
+                }
+            }
+        },
+        {
+            $project: {
+                owner: 1,
+                title: 1,
+                description: 1,
+                videoFile: 1,
+                views: 1
+            }
+        }
+    ]);
+
+    if (!(video.length)) throw new ApiError(404, "Video not found");
+
+    req?.user?.watchHistory?.push(videoId);
+    await req?.user?.save();
+    await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $inc: { views: 1 }
+        }
     )
-})
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                "Video fetched successfully",
+                video[0]
+            )
+        )
+});
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,12 +4,10 @@ const subscriptionSchema = new mongoose.Schema({
     subscriber: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true
     },
     channel: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true
     }
 },
 {
